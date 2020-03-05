a5d0de681bfeed5be7d519f2faef110b793ec083
Allef Sousa <allefsousa_1@hotmail.com>
2020-03-05 01:11:34 -0300

    Animated container

--- lib/main.dart
+++ lib/main.dart
@@ -27,48 +27,64 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var tamanho = 40.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Column(
-              children: <Widget>[
-                buttonsList(),
-                FlatButton(onPressed: (){}, child: Text("Button Flat")),
-                FlatButton(onPressed: (){}, child: Text("Button Flat 1")),
-                OutlineButton(onPressed: (){}, child: Text("Button Outline ")),
-                OutlineButton(onPressed: (){}, child: Text("Button Outline 1")),
-
-                Wrap(
-                  direction: Axis.horizontal,
-                  spacing: 35.0,
-                  runSpacing: 20.0,
-                  children: <Widget>[
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 2")),
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-                    RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-                  ],
-                )
-              ],
-            ),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          buttonsList(),
+          FlatButton(onPressed: () {}, child: Text("Button Flat")),
+          FlatButton(onPressed: () {}, child: Text("Button Flat 1")),
+          OutlineButton(onPressed: () {}, child: Text("Button Outline ")),
+          OutlineButton(onPressed: () {}, child: Text("Button Outline 1")),
+          Wrap(
+            direction: Axis.horizontal,
+            spacing: 35.0,
+            runSpacing: 20.0,
+            children: <Widget>[
+              RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+              RaisedButton(onPressed: () {}, child: Text("Buttom 2")),
+              RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+              RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+              RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+              RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+              RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+            ],
+          ),
+          AnimatedContainer(
+            duration: Duration(seconds: 5),
+            width: 60.0,
+            height: tamanho,
+            color: Colors.amber,
+          ),
+          RaisedButton(onPressed: () {
+            setState(() {
+              tamanho = 100.0;
+            });
+          }, child: Text("Buttom Update Conatiner")),
+        ],
+      ),
     );
   }
 }
 
-  Widget buttonsList(){
-    return ButtonBar(
-      alignment: MainAxisAlignment.center ,
-      children: <Widget>[
-        RaisedButton(onPressed: () {}, child: Text("Buttom 1"),),
-        RaisedButton(onPressed: () {}, child: Text("Buttom 2")),
-        RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
-        RaisedButton(onPressed: () {}, child: Text("Buttom 4")),
-      ],
-    );
+Widget buttonsList() {
+  return ButtonBar(
+    alignment: MainAxisAlignment.center,
+    children: <Widget>[
+      RaisedButton(
+        onPressed: () {},
+        child: Text("Buttom 1"),
+      ),
+      RaisedButton(onPressed: () {}, child: Text("Buttom 2")),
+      RaisedButton(onPressed: () {}, child: Text("Buttom 3")),
+      RaisedButton(onPressed: () {}, child: Text("Buttom 4")),
+    ],
+  );
 }
+
